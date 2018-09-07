dd75c4fe57811a3f51599d480188e475d99d5449
webaifei <1334806067@qq.com>
2018-09-07 22:51:30 +0800

    1. 完成 list add功能

--- bin/index.js
+++ bin/index.js
@@ -15,7 +15,8 @@
 const program = require('commander');
 const readline = require('readline');
 const fs = require('fs');
-
+const Table = require("cli-table");
+const os = require("os");
 
 var WINDOWS = process.platform === 'win32'
 var EOL = WINDOWS ?
@@ -26,13 +27,26 @@ const HOSTS = WINDOWS ?
   'C:/Windows/System32/drivers/etc/hosts' :
   '/etc/hosts';
 
-readHost();
+// readHost();
 // 输出命令
 
 program
+  // list
+  .command('list')
+  .action(function () {
+    readHost().then(res=> showAll(res));
+  })
+program
   .command('add <host> [ip]')
-  .action(function (host, ip) {
-
+  .action(function (host, ip="127.0.0.1") {
+    console.log(host);
+    readHost()
+      .then(res=> addHostIp(res, {host, ip}))
+      .then(()=> {
+        console.log(`add ${host}=>${ip} successfully!`);
+      }).catch(err=> {
+        console.log("add failly for: ",err);
+      });
   })
 
 program.parse(process.argv)
@@ -42,9 +56,88 @@ function readHost() {
   const rl = readline.createInterface({
     input: fs.createReadStream(HOSTS)
   });
-
-  rl.on('line', (line) => {
-    console.log('Line from file:', line);
+  const res = [];
+  return new Promise(function (resolve, reject) {
+    rl.on('line', (line) => {
+      console.log('Line from file:', line);
+      var item;
+      if(item = parseLine(line)) {
+        res.push(item);
+      }
+    }).on('close', function () {
+      // console.log(res);
+      resolve(res);
+    });
   });
-
 }
+/**
+ * write content to the host file
+ * @param Array list
+ */
+function writeHost(list) {
+  let lines = "";
+  list.forEach(item => {
+    if(!item.enable) {
+      lines += `# ${item.ip} ${item.host}`;
+    } else {
+      lines += `${item.ip} ${item.host}`;
+    }
+    lines += os.EOL;
+  });
+  // console.log(lines.length, 'lines');
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(HOSTS, lines, function (err) {
+      if(err) {
+        reject(err);
+      } else {
+        resolve({ok: true});
+      }
+    });
+  });
+}
+
+function parseLine(line) {
+  // ## 127.0.0.1 xxx.card.com
+  var item = {};
+  var validHostReg = /^(#+)?\d{3}\..+/;
+
+  if(validHostReg.test(line)) {
+    if(/^#/.test(line)) {
+      item.enable = false;
+    } else {
+      item.enable = true;
+    }
+    var ipAndHost = line.replace('#', '').split(/\s+/);
+    item.ip = ipAndHost[0];
+    item.host = ipAndHost[1];
+
+    return item;
+  } else {
+    return false;
+  }
+}
+/**
+ * show all the ip-host list
+ * @param Array list 
+ */
+function showAll(list) {
+  const table = new Table({
+    head: ['status','ip', 'host']
+  });
+  const newlist = list.map(item=> Object.values(item));
+  // console.log(newlist);
+  table.push(...newlist);
+  console.log(table.toString());
+}
+/**
+ * add host to the host file
+ * @param Array list
+ * @param Object addItem
+ */
+function addHostIp(list, addItem) {
+  const hasSameHost = list.filter(item=> item.host === addItem.host);
+  addItem.enable = true;
+  list.push(addItem);
+  return writeHost(list);
+}
+
