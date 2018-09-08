ad8d1f7b62bd2d50d80fbef1e292eaba4af3302a
webaifei <1334806067@qq.com>
2018-09-09 00:00:29 +0800

    完成功能

--- bin/index.js
+++ bin/index.js
@@ -17,41 +17,117 @@ const readline = require('readline');
 const fs = require('fs');
 const Table = require("cli-table");
 const os = require("os");
+const chalk = require("chalk");
+const chmod = require("chmod");
+const logSymbols = require("log-symbols");
+const cfonts = require("cfonts");
+
 
 var WINDOWS = process.platform === 'win32'
 var EOL = WINDOWS ?
   '\r\n' :
   '\n'
-
+const VERSION = require('../package.json').version;
+const APP_NAME = require('../package.json').name;
 const HOSTS = WINDOWS ?
   'C:/Windows/System32/drivers/etc/hosts' :
   '/etc/hosts';
 
-// readHost();
+
 // 输出命令
 
 program
-  // list
+  .version(VERSION)  
   .command('list')
+  .description("list all the configurated host")
   .action(function () {
-    readHost().then(res=> showAll(res));
+    readHost().then(res => showAll(res));
   })
+// add host => ip
 program
   .command('add <host> [ip]')
-  .action(function (host, ip="127.0.0.1") {
-    console.log(host);
+  .description('add [ip => host] to the hosts')
+  .action(function (host, ip = "127.0.0.1") {
     readHost()
-      .then(res=> addHostIp(res, {host, ip}))
-      .then(()=> {
-        console.log(`add ${host}=>${ip} successfully!`);
-      }).catch(err=> {
-        console.log("add failly for: ",err);
+      .then(res => addHostIp(res, {
+        host,
+        ip
+      }))
+      .then(() => {
+        console.log(logSymbols.success, chalk.red.bold(`add ${host}=>${ip} successfully~`));
+      }).catch(err => {
+        console.log(logSymbols.error, chalk.bold.red(`failed to add ${host}=>${ip}`));
+        console.log(`Error is ${err}`);
       });
+  })
+// remove host from the hosts
+program
+  .command('remove <host>')
+  .description('remove host from the hosts')
+  .action(function (host) {
+    readHost()
+      .then(res => removeHost(res, host))
+      .then(() => {
+        console.log(logSymbols.success, chalk.red.bold(`remove ${host} successfully~`));
+      }).catch(err => {
+        console.log(logSymbols.error, chalk.bold.red(`failed to remove ${host}`));
+        console.log(`Error is ${err}`);
+      });
+  })
+
+// enable the target host
+program
+  .command('enable <host>')
+  .description('enable the target host')
+  .action(function (host) {
+    readHost()
+      .then(res => enableHost(res, host))
+      .then(() => {
+        console.log(logSymbols.success, chalk.red.bold(`enable ${host} successfully~`));
+      }).catch(err => {
+        console.log(logSymbols.error, chalk.bold.red(`failed to enable ${host}`));
+        console.log(`Error is ${err}`);
+      });
+  })
+// disable the target host
+program
+  .command('disable <host>')
+  .description('disable the target host')
+  .action(function (host) {
+    readHost()
+      .then(res => disableHost(res, host))
+      .then(() => {
+        console.log(logSymbols.success, chalk.red.bold(`disable ${host} successfully~`));
+      }).catch(err => {
+        console.log(logSymbols.error, chalk.bold.red(`failed to disable ${host}`));
+        console.log(`Error is ${err}`);
+      });
+  })
+// chmod 666 on your hosts file
+program
+  .command('no-pwd')
+  .description('chmod 666 on your hosts file so you don\'t need input pwd')
+  .action(function (host) {
+    const error = chmod(HOSTS, 666);
+    if(error) {
+      console.log(logSymbols.error, chalk.bold.red(`failed to chmod ${HOSTS}`));
+      console.log(`Error is ${error}`);
+    } else {
+      console.log(logSymbols.success, chalk.red.bold(`never need input pwd~`));
+    }
   })
 
 program.parse(process.argv)
 
+// no sub command 
+if (!process.argv.slice(2).length) {
+  cfonts.say(APP_NAME)
+}
 
+/**
+ * read hosts content
+ * @returns Promise
+ */
 function readHost() {
   const rl = readline.createInterface({
     input: fs.createReadStream(HOSTS)
@@ -59,13 +135,12 @@ function readHost() {
   const res = [];
   return new Promise(function (resolve, reject) {
     rl.on('line', (line) => {
-      console.log('Line from file:', line);
       var item;
-      if(item = parseLine(line)) {
+      if (item = parseLine(line)) {
         res.push(item);
       }
     }).on('close', function () {
-      // console.log(res);
+      // console.log(res, 'res');
       resolve(res);
     });
   });
@@ -77,39 +152,45 @@ function readHost() {
 function writeHost(list) {
   let lines = "";
   list.forEach(item => {
-    if(!item.enable) {
+    if (!item.enable) {
       lines += `# ${item.ip} ${item.host}`;
     } else {
       lines += `${item.ip} ${item.host}`;
     }
     lines += os.EOL;
   });
-  // console.log(lines.length, 'lines');
+  // 
   return new Promise(function (resolve, reject) {
     fs.writeFile(HOSTS, lines, function (err) {
-      if(err) {
+      if (err) {
         reject(err);
       } else {
-        resolve({ok: true});
+        resolve({
+          ok: true
+        });
       }
     });
   });
 }
-
+/**
+ * parse hosts contents'line
+ * @param {String} line one line of hosts file content
+ */
 function parseLine(line) {
   // ## 127.0.0.1 xxx.card.com
   var item = {};
-  var validHostReg = /^(#+)?\d{3}\..+/;
-
-  if(validHostReg.test(line)) {
-    if(/^#/.test(line)) {
+  var validHostReg = /^(#+)?\s*\d{3}\..+/;
+  // console.log(line, "line");
+  if (validHostReg.test(line)) {
+    if (/^#/.test(line)) {
       item.enable = false;
     } else {
       item.enable = true;
     }
-    var ipAndHost = line.replace('#', '').split(/\s+/);
-    item.ip = ipAndHost[0];
+    var ipAndHost = line.replace('#', '').replace(/(^\s+|\s+$)/, '').split(/\s+/);
+
     item.host = ipAndHost[1];
+    item.ip = ipAndHost[0];
 
     return item;
   } else {
@@ -122,12 +203,12 @@ function parseLine(line) {
  */
 function showAll(list) {
   const table = new Table({
-    head: ['status','ip', 'host']
+    head: ['status', 'host', 'ip']
   });
-  const newlist = list.map(item=> Object.values(item));
+  const newlist = list.map(item => Object.values(item));
   // console.log(newlist);
   table.push(...newlist);
-  console.log(table.toString());
+  console.log(chalk.yellow(table.toString()));
 }
 /**
  * add host to the host file
@@ -135,9 +216,47 @@ function showAll(list) {
  * @param Object addItem
  */
 function addHostIp(list, addItem) {
-  const hasSameHost = list.filter(item=> item.host === addItem.host);
+  const hasSameHost = list.filter(item => item.host === addItem.host);
   addItem.enable = true;
   list.push(addItem);
   return writeHost(list);
 }
 
+/**
+ * remove all lines from hosts
+ */
+function removeHost(res, host) {
+  const list = res.filter(item => item.host !== host);
+
+  return writeHost(list);
+}
+
+/**
+ * enable target host
+ * @param {Array} res hosts list
+ * @param {String} host which host youo will enable 
+ */
+function enableHost(res, host) {
+  const list = res.map(item => {
+    if (item.host === host) {
+      item.enable = true;
+    }
+    return item;
+  })
+  return writeHost(list);
+}
+
+/**
+ * disable target host
+ * @param {Array} res hosts list
+ * @param {String} host which host youo will disable 
+ */
+function disableHost(res, host) {
+  const list = res.map(item => {
+    if (item.host === host) {
+      item.enable = false;
+    }
+    return item;
+  })
+  return writeHost(list);
+}
